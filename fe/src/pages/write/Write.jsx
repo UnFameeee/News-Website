4c04d7aab4060d1de58cb727f60079af054bf5c1
Quốc Thắng <quocthang036@gmail.com>
2022-04-13 20:42:15 +0700

    add setting module and posting module
    add setting module and posting module

--- fe/src/pages/write/Write.jsx
+++ fe/src/pages/write/Write.jsx
@@ -3,10 +3,15 @@ import "./write.css"
 export default function Write(){
     return ( 
         <div className='write'>
+            <img
+                className="writeImg"
+                src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+            />
             <div className="writeForm">
                 <div className="writeFormGroup">
                     <label htmlFor="fileInput">
-                        <i class="fa-solid fa-plus"></i>
+                        <i className="writeIcon fa-solid fa-plus"></i>
                     </label>
                     <input type="file" id="fileInput" style={{display: "none"}}/>
                     <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
@@ -14,7 +19,7 @@ export default function Write(){
                 <div className="writeFormGroup">
                     <textarea placeholder="Tell your story..." type="text" className="writeInput writeText" ></textarea>
                 </div>
-                <button className="writeSubmit"></button>
+                <button className="writeSubmit">Submit</button>
             </div>
         </div>
     )
