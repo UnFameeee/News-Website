7bc9d8ec3b1da5393bb8a134eba1892f6298d2ea
Quốc Thắng <quocthang036@gmail.com>
2022-05-03 23:11:58 +0700

    creating nodeJS BE - finishing FE
    creating nodeJS BE - finishing FE

---
+++ api/routes/auth.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+
+//REGISTER
+router.post("/register", async(req, res)=>{
+    try{
+        //encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt)
+
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass
+        })
+
+        const user = await newUser.save();
+        res.status(500).json(user)
+        
+    }catch(err){
+        //We didn't make exception handler
+        //so just return Error500 -> sthg wrong with Database
+        res.status(500).json(err)
+    }
+})
+
+
+//LOGIN
+router.post("/login", async(req, res) => {
+    try{
+        const user = await User.findOne({ username: req.body.username });
+        if(!user){
+            res.status(400).json("Wrong credential!");
+        }
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        if(!validated){
+            res.status(400).json("Wrong credential!");
+        }
+        else{
+            const{ password, ...others } = user._doc;
+            res.status(200).json(user);
+        }
+        
+    } catch(err){
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router
--- fe/src/component/topbar/TopBar.jsx
+++ fe/src/component/topbar/TopBar.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar(){
-    const user = false;
+    const user = true;
     return ( 
         <div className='top'>
             <div className="topLeft">
