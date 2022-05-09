6674618e0f52a9687a490ec3a72af49ecb889ae4
Quốc Thắng <quocthang036@gmail.com>
2022-05-09 20:08:48 +0700

    last published
    last published

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -10,7 +10,6 @@ router.post("/register", async(req, res)=>{
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt)
 
-
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
